db9339c37c22f1063beb7d787e1cea292a62e5b1
pannacottaaaaa <mrjccaranay@mymail.mapua.edu.ph>
2024-06-03 12:37:56 +0800

    Delete Dormie/front-end directory

--- Dormie/front-end/README.md
+++
@@ -1,2 +0,0 @@
-# dormie
-Dormie v2.0 Repository for CS155-2L
--- Dormie/front-end/process-register.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once 'index-db.php';
-	global $db;
-	session_start();
-	
-	
-	echo "{$username} {$email} {$password}"
-?>
